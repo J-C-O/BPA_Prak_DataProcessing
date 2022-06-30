0bbd26846bcf69e8a9376a4e8627967cd6906a5e
J-C-O <47179422+J-C-O@users.noreply.github.com>
2022-06-30 20:11:25 +0200

    changes#1

--- Program.cs
+++ Program.cs
@@ -14,15 +14,16 @@ Random rand = new Random();
 int simulatedProcessTime;
 
 // Device-Data
-string iotHubHostName = "";
-string deviceKey = "";
-string deviceId = "";
+// TASK-1: fill the variables with your data from your created Azure IoT-Hub
+string iotHubHostName = "...";
+string deviceKey = "...";
+string deviceId = "...";
 
-// create a new Object of the type DeviceAuthenticationWithRegistrySymmetricKey
+// TASK-2: create a new Object of the type DeviceAuthenticationWithRegistrySymmetricKey
 // use the DeviceAuthenticationWithRegistrySymmetricKey Constructor that takes the deviceId and the deviceKey
 var deviceAuthentication = new Object();
 
-// create a new Object of the type DeviceClient 
+// TASK-3: create a new Object of the type DeviceClient 
 // use for this the Create()-Method that takes the iotHubHostName, deviceAuthentication and Mqtt as TransportType
 // for more Info see: https://docs.microsoft.com/en-us/dotnet/api/microsoft.azure.devices.client.deviceclient.create?view=azure-dotnet#microsoft-azure-devices-client-deviceclient-create(system-string-microsoft-azure-devices-client-iauthenticationmethod-microsoft-azure-devices-client-transporttype) 
 DeviceClient deviceClient = DeviceClient.Create();
